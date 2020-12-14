6ab47ec1c9c74d8746b4a32145510566d33021ee
Simone Cardinale <simonecardinale8@gmail.com>
2020-12-14 17:31:11 +0100

    work complete + bonus + arrotondamento

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var tariffa = 0.21;
 var prezzo = (chilometri*tariffa).toFixed(2); //prezzo standard
 
 if (eta < 18){
-    prezzo=prezzo-(prezzo*20/100).toFixed(2);
+    prezzo=(prezzo-(prezzo*20/100)).toFixed(2);
 } else if (eta > 65) {
     prezzo=(prezzo-(prezzo*40/100)).toFixed(2);
 }
